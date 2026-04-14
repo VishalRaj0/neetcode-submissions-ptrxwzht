7de6a0bb528d5689a0d5e039b7bf39c2ba45f776
VishalRaj0 <108023331+VishalRaj0@users.noreply.github.com>
2026-04-15 00:27:09 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = defaultdict(int)
+        for c in s:
+            count[c] += 1
+        for c in t:
+            count[c] -= 1
+
+        for k, v in count.items():
+            if v != 0:
+                return False
+        return True
