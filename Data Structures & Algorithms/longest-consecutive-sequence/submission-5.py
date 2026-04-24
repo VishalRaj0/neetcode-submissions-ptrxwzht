1e6a9ac2f721a299b68fbbfb76615334215bcfcc
VishalRaj0 <108023331+VishalRaj0@users.noreply.github.com>
2026-04-25 03:34:43 +0530

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return len(nums)
+
+        import heapq
+        minHeap = []
+
+        for n in nums:
+            heapq.heappush(minHeap, n)
+        
+        res = 0
+        prev = float('-inf')
+        cur = 0
+        while minHeap:
+            pop = heapq.heappop(minHeap)
+            if prev + 1 == pop:
+                cur += 1
+            elif prev == pop:
+                continue
+            else:
+                cur = 1
+            res = max(res, cur)
+            prev = pop
+        
+        return res
