74c0261e845ea3036e989f2b1e558578f1ef3f5b
VishalRaj0 <108023331+VishalRaj0@users.noreply.github.com>
2026-05-04 02:47:27 +0530

    Add: minimum-window-with-characters - submission-7

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-7.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        matches = 0
+        tcount = defaultdict(int)
+        window = defaultdict(int)
+
+        for c in t:
+            tcount[c] += 1
+        count_to_match = len(tcount)
+
+        res = None
+        reslen = float('inf')
+        i = 0
+        for j in range(len(s)):
+            window[s[j]] += 1
+            if window[s[j]] == tcount[s[j]]:
+                matches += 1
+                if matches >= count_to_match:
+                    while matches == count_to_match:
+                        if (j - i) < reslen:
+                            res = [i, j]
+                            reslen = j - i + 1
+                        window[s[i]] -= 1
+                        if window[s[i]] < tcount[s[i]]:
+                            matches -= 1
+                        i += 1
+        
+        return s[res[0] : res[1] + 1] if res else ""
+
+        
